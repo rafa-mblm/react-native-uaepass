3752c1cf0db59180a6a12e5f945b0553ed4ed200
Rcarvalho <110898291+rafa-mblm@users.noreply.github.com>
2024-01-24 14:06:06 +0400

    Disable zoom in effect  on window focus

--- ios/UAEPassViewController.swift
+++ ios/UAEPassViewController.swift
@@ -10,10 +10,10 @@ import UAEPassClient
 class UAEPassViewController: UIViewController {
   override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
-    
+
     UAEPASSRouter.shared.spConfig = SPConfig(redirectUriLogin: UAEPass.redirectURL!,
-                                             scope: "urn:uae:digitalid:profile",
-                                             state: randomString(length: 24),  //Randomly Generated Code 24 alpha numeric.
+                                             scope: "urn:uae:digitalid:profile:general",
+                                             state: "HnlHOJTkTb66Y5H",  //Randomly Generated Code 24 alpha numeric.
                                              successSchemeURL: UAEPass.scheme + "://" + UAEPass.successHost!, //client success url scheme.
                                              failSchemeURL: UAEPass.scheme + "://" + UAEPass.failureHost!, //client failure url scheme.
                                              signingScope: UAEPass.scope!) // client signing scope.
@@ -22,18 +22,21 @@ class UAEPassViewController: UIViewController {
     login()
 
   }
-    
+
   func randomString(length: Int) -> String {
     let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
     return  String((0..<length).map{ _ in letters.randomElement()! })
   }
-   
+
 
   func login() {
-    
+
     if let webVC = UAEPassWebViewController.instantiate() as? UAEPassWebViewController {
-      
+
       webVC.urlString = UAEPassConfiguration.getServiceUrlForType(serviceType: .loginURL)
+
+      print(webVC.urlString)
+
       //print(webVC.urlString)
       let topViewController = UIApplication.shared.windows.last { $0.isKeyWindow }?.rootViewController
 //      if let topViewController = UserInterfaceInfo.topViewController() {
@@ -42,6 +45,8 @@ class UAEPassViewController: UIViewController {
           if let code = code {
             var returnData = [String: String]()
             returnData["accessCode"] = code
+            returnData["isSuccess"] = "true"
+            returnData["url"] = webVC.urlString
             UAEPass.resolveResponse(returnData)
           }else{
             let error = NSError(domain: "", code: 400)
@@ -54,14 +59,32 @@ class UAEPassViewController: UIViewController {
           UAEPass.rejectResponse("ERROR", response, error)
         }
         webVC.reloadwithURL(url: webVC.urlString)
+
+
+
+        let configuration = WKWebViewConfiguration()
+        let source: String = "var meta = document.createElement('meta');" +
+            "meta.name = 'viewport';" +
+            "meta.content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no';" +
+            "var head = document.getElementsByTagName('head')[0];" +
+            "head.appendChild(meta);"
+        let script: WKUserScript = WKUserScript(source: source, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+        webVC.webView?.configuration.userContentController.addUserScript(script)
+
+        webVC.webView?.isMultipleTouchEnabled = false;
+        webVC.webView?.contentMode = .scaleAspectFit
+        webVC.webView?.scrollView.minimumZoomScale = 1.0
+        webVC.webView?.scrollView.maximumZoomScale = 1.0
+        webVC.webView?.scrollView.bounces = false
+
         self.present(webVC, animated: true)
 //      }
     }
   }
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
 }
